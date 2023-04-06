5911a6e38053415b58a21dbf30efa9401cc735c7
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2023-04-06 17:04:37 +0530

    Add files via upload

---
+++ Adventure Trip.py
@@ -0,0 +1,39 @@
+name=input("Enter your Name Beautiful Girl:")
+print("Hello",""+ name +" "+"Welcome to this Journey!!!!")
+answer=input("Are you ready for the amazing journey with us???\n(Left Way/Right Way)")
+if answer =="Left Way":
+   answer=input("You went to a Fairy Tale Castle....(They ask you to dress like them?\n(yes/no)")
+if answer=="yes":
+        answer=input("Would you like to dess up like Disney Princess....\n((1)Snow-White!/(2)Barbie!(3)Elsa!(4)Cinderella!")
+if answer=="Snow-White":
+          answer=input("Congratulation ! You are looking like Pretty Snow-White with Red Apple....now you can chose any number see what power do u get\none \ntwo \nthree \nfour??")
+if answer=="Barbie":
+                 answer=input("Amazing ! You are looking like gorgeous,Just like Elsa....now you can chose any number see what power do u get\none \ntwo \nthree \nfour??")
+if answer=="Elsa":
+                 answer=input("Astonishing ! You proving that any girl can become a princess like Elsa, too!....now you can chose any number see what power do u get\none \ntwo \nthree \nfour??")
+if answer=="Cinderella":
+                 answer=input("Stunning.! You remain beautiful as well as you keep your  dreams and remains a kind, gentle and sweet person just like Cinderella.....now you can chose any number see what power do u get \none \ntwo \nthree \nfour??")
+if answer=="one":
+                print("shocking,Now you can read anyone mind!!!")
+if answer=="two":
+                    print("wonderful,Got the power to fly like birds!!!!")
+if answer=="three":
+                        print("Unexpected, You have the option to love any girl's whom you want!!!!.")
+if answer=="four":
+                        print("Wowww, You have the option to eat anything!!!!.")
+if answer=="no":
+            ("You get the chance to have magical powers!!(1)Have a power to read anyones mind\n (2)Have a power to fly\n(3)Have a power to love anygirl's\n")
+if answer=="1":
+                print("shocking,Now you can read anyone mind!!!")
+if answer=="2":
+            print("wonderful,Got the power to fly like birds!!!!")
+if answer=="3":
+             print("Unexpected, You have the option to love any girl's whom you want!!!!.")
+if answer =="Right Way":
+                answer=input("  You can go to Topsy-turvy Land\n chose School\n or Garden\n")
+if answer=="school":
+                    print("Bored!! Go and study there in night.....")
+if answer=="Garden":
+                print("WOwww!! Can have fun with friends whole day....")
+               
+print("You Lose the game.Thanks for coming")
