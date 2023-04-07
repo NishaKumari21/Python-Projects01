375ed972be396a5ac7aebed38364d389f2d771c6
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2023-04-07 19:14:50 +0530

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -1,3 +1,4 @@
+#First you need to install any python interpreter for running code or you can use online compiler too.
 #This is the first program to be runned by every python programmer.
 print("Hello World")
 #The output after the code will execute will be Hello World.
